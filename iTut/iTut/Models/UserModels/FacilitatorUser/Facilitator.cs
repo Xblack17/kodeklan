9fe48eb142a1e28616e1f6f4b07ab9f6dc25b8ee
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-03-19 10:17:10 +0200

    Feature: Base Users' Models

---
+++ iTut/iTut/Models/UserModels/FacilitatorUser/Facilitator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iTut.Models.UserModels.FacilitatorUser
+{
+    public class Facilitator
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; } = $"{Guid.NewGuid()}{Guid.NewGuid()}";
+
+        [Required]
+        public string UserId { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool Archived { get; set; }
+    }
+}
---
+++ iTut/iTut/Models/UserModels/ParentUser/Parent.cs
@@ -0,0 +1,36 @@
+﻿using iTut.Models.UserModels.StudentUser;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iTut.Models.UserModels.ParentUser
+{
+    public class Parent
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public string Id { get; set; } = $"{Guid.NewGuid()}{Guid.NewGuid()}";
+
+        [Required]
+        public string UserId { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        public Gender Gender { get; set; }
+
+        public Race Race { get; set; }
+
+        public string PhysicalAddress { get; set; }
+
+        public List<Student> Children { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public bool Archived { get; set; }
+    }
+}
