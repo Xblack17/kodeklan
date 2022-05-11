119ba67f4bcfb0feab7ac6225644ba81935c9b5f
Johannes Mogashoa <jomogashoa1993@gmail.com>
2022-05-11 23:34:06 +0200

    Added Assets & Structure

---
+++ iTut/wwwroot/js/scripts.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('.offcanvas-button').click(function () {
+        $('.offcanvas-collapse').toggleClass('open');
+    });
+
+    $('.logout-button').click(function () {
+        localStorage.clear();
+        window.location.href = "/Account/Logout";
+    });
+});
